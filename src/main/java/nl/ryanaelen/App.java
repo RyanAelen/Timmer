d67eae5c2413869aa90048dc3fa2abe98b078ad4
RyanAelen <rmm.aelen@student.han.nl>
2021-02-08 22:45:30 +0100

    maven project setup

---
+++ src/main/java/nl/ryanaelen/App.java
@@ -0,0 +1,41 @@
+package nl.ryanaelen;
+
+import processing.core.PApplet;
+
+/**
+ * @author Ryan Aelen
+ */
+public class App extends PApplet {
+    /**
+     *
+     */
+    private static final int HEIGHT = 400;
+    /**
+     *
+     */
+    private static final int WIDTH = 400;
+
+    /**
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        PApplet.main("nl.ryanaelen.App", args);
+    }
+
+    @Override
+    public void setup() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void settings() {
+        this.size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void draw() {
+        // TODO Auto-generated method stub
+
+    }
+}
